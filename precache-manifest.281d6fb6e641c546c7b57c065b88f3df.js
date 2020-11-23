8f564c943682d955d08bd525738ba60ff2c8c7c4
Valentin <qweqweq@qwe>
2020-11-23 17:33:48 +0300

    Updates

---
+++ precache-manifest.281d6fb6e641c546c7b57c065b88f3df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a1be16d57a2c75c61ecb296f5e14ea",
+    "url": "/react-countries/index.html"
+  },
+  {
+    "revision": "008185ca064bf639b083",
+    "url": "/react-countries/static/css/main.6c39c37b.chunk.css"
+  },
+  {
+    "revision": "4203056388f11df5b97a",
+    "url": "/react-countries/static/js/2.6b8140fb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-countries/static/js/2.6b8140fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "008185ca064bf639b083",
+    "url": "/react-countries/static/js/main.3221ea68.chunk.js"
+  },
+  {
+    "revision": "423ac27bd6389194b487",
+    "url": "/react-countries/static/js/runtime-main.1a3475b1.js"
+  },
+  {
+    "revision": "b0d773cf3c10b939977ff4f623dc8579",
+    "url": "/react-countries/static/media/search-solid.b0d773cf.svg"
+  }
+]);
